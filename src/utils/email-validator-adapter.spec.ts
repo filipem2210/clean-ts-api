80894e42e0bd19303251f2058348bec40a8f8337
Filipe Moraes <filipemoraes2210@gmail.com>
2021-01-05 20:44:46 -0300

    fix: change toHaveBeenLastCalledWith to toHaveBeenCalledWith

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -29,6 +29,6 @@ describe('EmailValidator Adapter', () => {
     const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     sut.isValid('any_email@mail.com')
-    expect(isEmailSpy).toHaveBeenLastCalledWith('any_email@mail.com')
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
