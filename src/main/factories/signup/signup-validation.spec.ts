78a8eb45465ef7b841c37e78a273016a4f025c15
Filipe Moraes <filipemoraes2210@gmail.com>
2021-01-17 21:19:09 -0300

    refactor: move Validation to protocols folder

--- src/main/factories/signup/signup-validation.spec.ts
+++ src/main/factories/signup/signup-validation.spec.ts
@@ -1,7 +1,7 @@
 import { makeSignUpValidation } from './signup-validation'
 import { ValidationComposite } from '../../../presentation/helpers/validator/validation-composite'
 import { RequiredFieldValidation } from '../../../presentation/helpers/validator/required-field-validation'
-import { Validation } from '../../../presentation/helpers/validator/validation'
+import { Validation } from '../../../presentation/protocols/validation'
 import { CompareFieldsValidation } from '../../../presentation/helpers/validator/compare-fields-validation'
 import { EmailValidation } from '../../../presentation/helpers/validator/email-validation'
 import { EmailValidator } from '../../../presentation/protocols/email-validator'
--- src/main/factories/signup/signup-validation.ts
+++ src/main/factories/signup/signup-validation.ts
@@ -1,6 +1,6 @@
 import { ValidationComposite } from '../../../presentation/helpers/validator/validation-composite'
 import { RequiredFieldValidation } from '../../../presentation/helpers/validator/required-field-validation'
-import { Validation } from '../../../presentation/helpers/validator/validation'
+import { Validation } from '../../../presentation/protocols/validation'
 import { CompareFieldsValidation } from '../../../presentation/helpers/validator/compare-fields-validation'
 import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
 import { EmailValidation } from '../../../presentation/helpers/validator/email-validation'
