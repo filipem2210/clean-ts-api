fe85f8906f2341bbc619db6169f2fb5411996e5d
Filipe Moraes <filipemoraes2210@gmail.com>
2021-01-20 22:40:56 -0300

    refactor: remove extra line

--- src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -3,9 +3,7 @@ import { Hasher } from '../../../data/protocols/cryptography/hasher'
 import { HashComparer } from '../../../data/protocols/cryptography/hash-comparer'
 
 export class BcryptAdapter implements Hasher, HashComparer {
-  constructor (
-    private readonly salt: number
-  ) {}
+  constructor (private readonly salt: number) {}
 
   async hash (value: string): Promise<string> {
     const hash = await bcrypt.hash(value, this.salt)
